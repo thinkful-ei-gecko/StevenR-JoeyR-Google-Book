04504ec5c0932200139fdc43f0fbdafff2b7f2e3
Joey Romo <joeyjr95@gmail.com>
2019-09-10 12:36:33 -0400

    inital

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [],
+      selected: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://www.googleapis.com/books/v1/volumes?q=joey')
+      .then(response => response.json())
+      .then( data => {
+        this.setState({
+          books: [data]
+          
+        });
+        console.log(this.state.books)
+      })
+  }
+  setSelected(selected) {
+    this.setState([
+      selected
+    ]);
+    
+    
+  }
+
+  render() {
+    return(<section>
+      {/* < Form 
+        books={this.state.books}
+        search={this.state.selected}
+      /> */}
+      <section>
+        <div>{this.state.books}</div>
+        
+      </section>
+      
+    </section>)
+  }
 }
+
+
 
 export default App;
